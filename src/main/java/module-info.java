aaf9cef0e474cc03a2db538f521d1961c1dfca8f
benjasteck <benjasteckmetz@gmail.com>
2022-04-26 03:15:09 +0200

    ...

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.examsecondsemester {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.examsecondsemester to javafx.fxml;
-    exports com.example.examsecondsemester;
-}
